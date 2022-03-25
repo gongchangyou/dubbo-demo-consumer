75ce31c53f56c12b198a07afdc231e141bf9f451
gongchangyou <gongchangyou@58.com>
2022-03-25 10:22:45 +0800

    comment

--- src/main/java/com/braindata/rocketmq/Consumer.java
+++ src/main/java/com/braindata/rocketmq/Consumer.java
@@ -1,17 +1,17 @@
 package com.braindata.rocketmq;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 /**
  * @author gongchangyou
  * @version 1.0
  * @date 2022/1/26 5:00 下午
  */
+@Slf4j
 @Component
 public class Consumer implements CommandLineRunner {
 
@@ -29,9 +29,13 @@ public class Consumer implements CommandLineRunner {
      */
     @Override
     public void run(String... args) throws Exception {
-        System.out.printf("asdf");
-        while(true) {
-            messageSource.input1().subscribe(msg -> System.out.println("payload=" + new String((byte[])msg.getPayload())));
-        }
+        log.info("run");
+//        while(true) { //这里我也不知道为啥需要死循环，试过如果rocketMQ server挂掉，重启依然可以正常消费呀
+            messageSource.input1().subscribe(msg -> {
+                //这里如果抛异常的话，broker中的offset不会往前移，所以依然会pull到出错的那个消息,相当于 ack false了
+                log.info("payload=" + new String((byte[]) msg.getPayload()));
+
+            });
+//        }
     }
 }
